180e187929209b0405bfef358513d987828a0b73
Dan Ruth <danjruth@gmail.com>
2020-07-23 09:23:45 -0400

    update env367_tools to include optional argument for the directory for saving/loading files

--- oscar/env367_tools.py
+++ oscar/env367_tools.py
@@ -8,15 +8,53 @@ Created on Tue Jul 14 21:56:48 2020
 import numpy as np
 import xarray as xr
 
-res_folder = r'C:\Users\danjr\Documents\Teaching\ENV367_Fall2020\oscar-exercises\saved-data\\'
+# hard-coded path to the read-only data directory
+default_dir = r'C:\Users\danjr\Documents\Teaching\ENV367_Fall2020\oscar-exercises\saved-data\\'
 
-def save_run_model_output(output,name_start):
+def save_run_model_output(output,name_start,data_dir=default_dir):
+    '''
+    Save each DataSet of the run_model output dictionary as a .nc file. Only
+    saves values that are not None.
+    
+    
+    Parameters
+    ----------
+    output : dict
+        The dictionary output by run_model
+        
+    name_start : str
+        The start of the filename for each saved file
+        
+    data_dir : str, optional
+        The path to the directory in which the file is saved; defaults to a
+        directory that is read-only for students on Adroit
+    '''
+    
+    # save each dictionary value (which is not None) as an .nc file
     for key in output:
         if output[key] is not None:
-            output[key].to_netcdf(res_folder+name_start+'_'+key+'.nc', encoding={var:{'zlib':True, 'dtype':np.float32} for var in output[key]})
+            output[key].to_netcdf(data_dir+name_start+'_'+key+'.nc', encoding={var:{'zlib':True, 'dtype':np.float32} for var in output[key]})
         
-def load_model_output(name_start):
-    '''load saved data into a dictionary, as if the model was just run'''
+def load_model_output(name_start,data_dir=default_dir):
+    '''
+    Load model output that was saved in a dict (in the same format that
+    run_model returns)
+    
+    Parameters
+    ----------
+    name_start : str
+        The start of the filename for each of the saved files to load
+    
+    data_dir : str, optional
+        The path to the directory in which the file is saved; defaults to a
+        directory that is read-only for students on Adroit
+        
+    Returns
+    -------
+    loaded_model_output : dict
+        A dict that resembles the output of run_model
+    '''
+
     keys = ['Par',
             'For_hist',
             'Ini_hist',
@@ -27,7 +65,7 @@ def load_model_output(name_start):
     loaded_model_output = {}
     for key in keys:
         try:
-            fpath = res_folder+name_start+'_'+key+'.nc'
+            fpath = data_dir+name_start+'_'+key+'.nc'
             with xr.open_dataset(fpath) as TMP: x = TMP.load()
             loaded_model_output[key] = x
         except FileNotFoundError:
